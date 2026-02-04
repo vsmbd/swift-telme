d1d877213595172fbaa8e841a3b2198ce07ce0f5
vsmbd <vsmbd42f29@privaterelay.appleid.com>
2026-02-04 21:32:49 +0530

    Enhance ConsoleRecordSink to use JSON encoding for record output, improving readability and structure of printed logs.

--- Sources/Telme/ConsoleRecordSink.swift
+++ Sources/Telme/ConsoleRecordSink.swift
@@ -5,14 +5,40 @@
 //  Created by vsmbd on 30/01/26.
 //
 
+import Foundation
+
 // MARK: - ConsoleRecordSink
 
 public final class ConsoleRecordSink: TelmeRecordSink {
+	// MARK: + Private scope
+
+	private let encoder: JSONEncoder = {
+		let jsonEncoder = JSONEncoder()
+		jsonEncoder.outputFormatting = [
+			.prettyPrinted,
+			.sortedKeys
+		]
+		return jsonEncoder
+	}()
+
+	// MARK: + Public scope
+
 	public init() {
 		//
 	}
-	
+
 	public func sink(_ record: TelmeRecord) {
-		print(record)
+		do {
+			let data = try encoder.encode(record)
+
+			if let json = String(
+				data: data,
+				encoding: .utf8
+			) {
+				print(json)
+			}
+		} catch {
+			print(record)
+		}
 	}
 }
